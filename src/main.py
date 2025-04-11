caa65b5284e753ad0f182217e440b85b77e7c792
marhide <louis.smith@hyperoceanic.com>
2025-04-11 12:14:07 +0100

    main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,11 @@
 from configparser import ConfigParser
 import requests
 
-
 config = ConfigParser()
 config.read('secrets.ini')
 guardian_api_key = config['secrets']['guardian_api_key']
 
 url = 'https://content.guardianapis.com/search?api-key=' + guardian_api_key
-
-
-print(guardian_api_key)
-
 
 r = requests.get(url)
 print(r)
