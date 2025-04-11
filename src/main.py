d4aea1737e73ddd5e5491dc3c8b492788e486043
marhide <louis.smith@hyperoceanic.com>
2025-04-11 22:04:45 +0100

    tests for create request function

--- src/main.py
+++ src/main.py
@@ -2,23 +2,48 @@ from os import environ
 from src.init_env import init_environment
 import requests
 
-def get_request():
+def create_request(search_term=None, date_from=None):
+
+    query = ''
+
+    if search_term:
+        search_term = '?q=' + search_term.replace(' ', '%20')
+        query += search_term
+
+    if date_from:
+        date_from = '&from-date=' + date_from
+        query += date_from
+
+    url = environ['deafault_url'] + query
+
     request = (
-        environ['deafault_url'], 
+        url,
         {
             'api-key': environ['api_key'],
             'format': environ['response_format']
         }
     )
+
     return request
 
 
-def get_status_code():
-    request = get_request()
+def get_status_code(request=None):
+
+    if request == None:
+        request = create_request()
+
     response = requests.get(*request)
     return response.status_code
 
 
 if __name__ == '__main__':
     init_environment()
-    print(f'status code: {get_status_code()}')
+
+    search_term = 'machine learning'
+    date_from = '2023-01-01'
+
+    request = create_request(search_term, date_from)
+
+    status_code = get_status_code(request)
+
+    print(f'status code: {status_code}')
--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,47 @@
-from src.main import get_status_code
+from src.main import get_status_code, create_request
 from src.init_env import init_environment
 
 init_environment()
+
+#test create_request
+
+def test_create_request_returns_tuple():
+    request = create_request()
+    assert isinstance(request, tuple)
+
+def test_create_request_has_str_as_first_item():
+    request = create_request()
+    assert isinstance(request[0], str)
+
+def test_create_request_has_dict_as_second_item():
+    request = create_request()
+    assert isinstance(request[1], dict)
+
+def test_create_request_has_correct_query_url():
+    request = create_request()
+    expected = 'https://content.guardianapis.com/search'
+    assert request[0] == expected
+
+def test_create_request_has_correct_query_url_when_given_search_term():
+    search_term = 'machine learning'
+    request = create_request(search_term)
+    expected = 'https://content.guardianapis.com/search?q=machine%20learning'
+    assert request[0] == expected
+
+def test_create_request_has_correct_query_url_when_given_only_date_from():
+    date_from = '2023-01-01'
+    request = create_request(date_from=date_from)
+    expected = 'https://content.guardianapis.com/search&from-date=2023-01-01'
+    assert request[0] == expected
+
+def test_create_request_has_correct_query_url_when_given_search_term_and_date_from():
+    search_term = 'machine learning'
+    date_from = '2023-01-01'
+    request = create_request(search_term, date_from)
+    expected = 'https://content.guardianapis.com/search?q=machine%20learning&from-date=2023-01-01'
+    assert request[0] == expected
+
+#test get_response_code
 
 def test_get_response_code_returns_int():
     response = get_status_code()
