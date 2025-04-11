e957e5a55bedc6fe39856c2f4cca75e6ef9986f1
marhide <louis.smith@hyperoceanic.com>
2025-04-11 22:10:26 +0100

    test status code response when given request

--- test/test_main.py
+++ test/test_main.py
@@ -43,11 +43,18 @@ def test_create_request_has_correct_query_url_when_given_search_term_and_date_fr
 
 #test get_response_code
 
-def test_get_response_code_returns_int():
-    response = get_status_code()
-    assert isinstance(response, int)
+def test_get_status_code_returns_int():
+    status_code = get_status_code()
+    assert isinstance(status_code, int)
 
-def test_get_response_code_returns_200():
+def test_get_status_code_returns_200():
     excepted = 200
-    response = get_status_code()
-    assert response == excepted
+    status_code = get_status_code()
+    assert status_code == excepted
+
+def test_get_status_code_returns_200_when_given_query():
+    search_term = 'machine learning'
+    date_from = '2023-01-01'
+    request = create_request(search_term, date_from)
+    status_code = get_status_code(request)
+    assert status_code == 200
