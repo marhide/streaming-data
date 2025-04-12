6510d857097b3e8e2af8b22d9143d42d7814d9cb
marhide <louis.smith@hyperoceanic.com>
2025-04-12 01:36:12 +0100

    commit update

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,18 @@ from configparser import ConfigParser
 from pathlib import Path
 from os import environ
 
+
+def create_secret_config():
+
+    secret_config_path = Path('secret_config.ini')
+    
+    if not secret_config_path.exists():
+        new_api_key = input('No Guardian API key found!\nPlase enter your Guardian API key:')
+
+        with open('secret_config.ini', 'w', encoding='utf-8') as file:
+            file.write('[secrets]\nguardian_api_key = '+ new_api_key)
+
+
 def init_env_vars():
 
     config_files_to_read = [
@@ -15,12 +27,3 @@ def init_env_vars():
     environ['api_key'] = config['secrets']['guardian_api_key']
     environ['response_format'] = config['headers']['response_format']
     environ['deafault_url'] = config['url']['deafault_url']
-
-def create_secrets_config():
-    secrets_config_path = Path('secret_config.ini')
-    
-    if not secrets_config_path.exists():
-        new_api_key = input('No Guardian API key found!\nPlase enter your Guardian API key:')
-
-        with open('secret_config.ini', 'w', encoding='utf-8') as file:
-            file.write('[secrets]\nguardian_api_key = '+ new_api_key)
--- test/test_utils.py
+++ test/test_utils.py
@@ -9,18 +9,15 @@ running_locally = getenv("GITHUB_ACTIONS") == None
 def test_init_env_vars_creates_correct_enivronmental_deafault_url():
     expected_default_url = 'https://content.guardianapis.com/search'
     test_default_url = getenv('deafault_url')
-
     assert test_default_url == expected_default_url
 
 def test_init_env_vars_creates_correct_enivronmental_response_format():
     expected_response_format = 'json'
     test_response_format = getenv('response_format')
-
     assert test_response_format == expected_response_format
 
 @pytest.mark.skipif(running_locally, reason='test only checks for mock api key in github actions')
 def test_init_env_vars_creates_correct_enivronmental_api_key():
     expected_api_key = 'test'
     test_api_key = getenv('api_key')
-
     assert test_api_key == expected_api_key
