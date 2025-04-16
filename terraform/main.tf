25665ebca3371b58680a180fcd85f80d7cc5d962
marhide <louis.smith@hyperoceanic.com>
2025-04-16 04:52:56 +0100

    terraform change sqs delay times to 1 second

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 resource "aws_sqs_queue" "terraform_queue" {
   name                      = "my_queue"
-  delay_seconds             = 90
+  delay_seconds             = 1
   max_message_size          = 2048
   message_retention_seconds = 86400
-  receive_wait_time_seconds = 10
+  receive_wait_time_seconds = 1
 
 }
