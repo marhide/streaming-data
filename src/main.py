b8d1429509f2415ccf351fd06f616c30ab92d1ae
marhide <louis.smith@hyperoceanic.com>
2025-04-18 04:45:58 +0100

    deactivate fn created

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 from src.get_message_from_api_request import get_message, create_request
 from src.setup import setup_env
 
-
 import requests
 from pprint import pprint
 import boto3
 from os import getenv
-
+import os
 
 def get_status_code(request=None):
 
@@ -41,17 +40,49 @@ def get_queue(name):
     return queue
 
 
+def deactivate():
+
+    os.remove('secret_config.ini')
+    os.remove('./terraform/secrets.auto.tfvars')
+
+    environ_list = [
+        'api_key',
+        'queue_name',
+        'response_format',
+        'message_id',
+        'default_url',
+        'default_search_term',
+        'default_from_date'
+        ]
+
+    for item in environ_list:
+
+        try:
+            os.environ.pop(item)
+
+        except:
+            raise Exception(f'{item} has not been set as an environ')
+
+
 if __name__ == '__main__':
-    setup_env()
 
-    search_term = getenv('default_search_term')
-    from_date = getenv('default_from_date')
-    message_id = getenv('message_id')
+    try:
+        setup_env()
 
-    message = get_message(search_term, from_date)
+        search_term = getenv('default_search_term')
+        from_date = getenv('default_from_date')
 
-    queue_name = getenv('queue_name')
-    queue = get_queue(queue_name)
 
-    response = send_message_to_queue(queue, message)
-    pprint(response)
+        message = get_message(search_term, from_date)
+
+        queue_name = getenv('queue_name')
+        queue = get_queue(queue_name)
+
+        response = send_message_to_queue(queue, message)
+        pprint(response)
+
+    except:
+        raise Exception('Something has gone wrong.')
+    
+    finally:
+        deactivate()
