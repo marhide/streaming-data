a16b0c9967d95eaa4a7af0c3e90cfc53dbe204b8
marhide <louis.smith@hyperoceanic.com>
2025-04-25 01:05:52 +0100

    tests for testvalidatedate fixed

--- test/test_utils.py
+++ test/test_utils.py
@@ -3,21 +3,19 @@ import pytest
 from src.utils import validate_date
 
 class TestValidateDate():
-    def test_validate_date_raises_error_if_given_incorrect_date():
+    def test_validate_date_raises_error_if_given_incorrect_date(self):
         bad_test_date = "not a date"
         with pytest.raises(Exception):
             validate_date(bad_test_date)
 
-
-    def test_validate_date_does_not_raise_error_if_given_correct_date():
+    def test_validate_date_does_not_raise_error_if_given_correct_date(self):
         test_date = "1999-01-01"
         try:
             validate_date(test_date)
         except Exception as e:
             assert False, f"raised an exception {e}"
 
-
-    def test_validate_date_raises_error_if_given_incorrect_date_in_correct_format():
+    def test_validate_date_raises_error_if_given_incorrect_date_in_correct_format(self):
         suspicious_date = "2024-04-31"
         with pytest.raises(Exception):
             validate_date(suspicious_date)
