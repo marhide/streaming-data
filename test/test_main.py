05e603e8da40a6bc0237c255cd09b3743232e8c8
marhide <louis.smith@hyperoceanic.com>
2025-04-28 23:37:12 +0100

    test to see correct message gets sent to sqs queue

--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,6 @@
 import os
 from unittest import mock
+from pprint import pprint
 
 import boto3
 from moto import mock_aws
@@ -26,20 +27,39 @@ def test_get_queue_returns_queue_object():
         test_queue.url == "https://sqs.eu-west-2.amazonaws.com/123456789012/mock_queue"
     )
 
-@mock_aws
-def test_send_message_to_queue_returns_status_code_200_when_given_a_queue_obj_and_correct_message():
-    mock_sqs = boto3.resource("sqs")
-    test_queue_name = "mock_queue.fifo"
-    test_sqs_attributes = {"FifoQueue": "True", "ContentBasedDeduplication": "True"}
-    mock_sqs.create_queue(
-        QueueName=test_queue_name,
-        Attributes=test_sqs_attributes,
-    )
-    test_queue = get_queue(test_queue_name)
-    test_message = str({"header": "body"})
 
-    response = send_message_to_queue(test_queue, str(test_message))
-    assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+class TestSendMessageToQueue():
+    @mock_aws
+    def test_send_message_to_queue_returns_status_code_200_when_given_a_queue_obj_and_correct_message(self):
+        mock_sqs = boto3.resource("sqs")
+        test_queue_name = "mock_queue.fifo"
+        test_sqs_attributes = {"FifoQueue": "True", "ContentBasedDeduplication": "True"}
+        mock_sqs.create_queue(
+            QueueName=test_queue_name,
+            Attributes=test_sqs_attributes,
+        )
+        test_queue = get_queue(test_queue_name)
+        test_message = str({"header": "body"})
+
+        response = send_message_to_queue(test_queue, test_message)
+        assert response["ResponseMetadata"]["HTTPStatusCode"] == 200
+
+    @mock_aws
+    def test_send_message_to_queue_sends_the_correct_message_to_the_queue(self):
+        mock_sqs = boto3.resource("sqs")
+        sqs_client = boto3.client("sqs")
+        test_queue_name = "mock_queue.fifo"
+        test_sqs_attributes = {"FifoQueue": "True", "ContentBasedDeduplication": "True"}
+        mock_sqs.create_queue(
+            QueueName=test_queue_name,
+            Attributes=test_sqs_attributes,
+        )
+        test_queue = get_queue(test_queue_name)
+        test_message = str({"test_header": "test_body"})
+
+        send_message_to_queue(test_queue, test_message)
+        recieved_message = sqs_client.receive_message(QueueUrl=test_queue_name)
+        assert recieved_message['Messages'][0]['Body'] == test_message
 
 
 class TestInputSearchTerm():
