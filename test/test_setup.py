1fe39befd6f585961e2eff9c4338619e752bbcb2
marhide <louis.smith@hyperoceanic.com>
2025-04-29 21:29:47 +0100

    pytest fixure use refactor

--- test/test_setup.py
+++ test/test_setup.py
@@ -4,60 +4,60 @@ import pytest
 
 from src.setup import set_env_vars, set_secret_env_vars, create_secrets_tfvars_file, SetupEnv
 
+global test_api_key, test_queue_name
+test_api_key, test_queue_name  = 'test', 'test_queue_name'
 
 @pytest.fixture(scope='class', autouse=False)
 def run_set_env_vars():
     set_env_vars()
 
-
 @pytest.fixture(scope='class', autouse=False)
 def run_set_secret_env_vars():
-    set_secret_env_vars("test", "test_queue_name")
+    set_secret_env_vars(api_key=test_api_key, queue_name=test_queue_name)
 
 
+@pytest.mark.usefixtures('run_set_env_vars')
 class TestSetEnvVars():
-    def test_set_env_vars_creates_correct_enivronmental_response_format(self, run_set_env_vars):
+    def test_set_env_vars_creates_correct_enivronmental_response_format(self):
         expected_response_format = "json"
         test_response_format = os.getenv("response_format")
         assert test_response_format == expected_response_format
 
-    def test_set_env_vars_sets_correct_message_id(slef, run_set_env_vars):
+    def test_set_env_vars_sets_correct_message_id(self):
         expected = "guardian_content"
         test = os.getenv("message_id")
         assert test == expected
 
-    def test_set_env_vars_creates_correct_enivronmental_default_url(self, run_set_env_vars):
+    def test_set_env_vars_creates_correct_enivronmental_default_url(self):
         expected_default_url = "https://content.guardianapis.com/search?"
         test_default_url = os.getenv("default_url")
         assert test_default_url == expected_default_url
 
-    def test_set_env_vars_sets_correct_default_search_term(self, run_set_env_vars):
+    def test_set_env_vars_sets_correct_default_search_term(self):
         expected_default_search_term = "machine learning"
         test_default_search_term = os.getenv("default_search_term")
         assert expected_default_search_term == test_default_search_term
 
-    def test_set_env_vars_sets_correct_default_from_date(self, run_set_env_vars):
+    def test_set_env_vars_sets_correct_default_from_date(self):
         expected = "1900-01-01"
         test_default_from_date = os.getenv("default_from_date")
         assert test_default_from_date == expected
 
 
+@pytest.mark.usefixtures('run_set_secret_env_vars')
 class TestSetSecretEnvVars():
-
-    def test_set_secret_env_vars_creates_correct_enivronmental_api_key(self, run_set_secret_env_vars):
+    def test_set_secret_env_vars_creates_correct_enivronmental_api_key(self):
         expected_api_key = "test"
         test_api_key = os.getenv("api_key")
         assert test_api_key == expected_api_key
 
-
-    def test_secret_set_env_vars_creates_correct_env_q_name(self, run_set_secret_env_vars):
+    def test_secret_set_env_vars_creates_correct_env_q_name(self):
         expected_queue_name = "test_queue_name.fifo"
         test_queue_name = os.getenv("queue_name")
         assert test_queue_name == expected_queue_name
 
 
 class TestCreateSecretTfvarsFile:
-
     def test_create_tfvars_file_creates_a_file(self):
         create_secrets_tfvars_file()
         assert os.path.exists("./terraform/secrets.auto.tfvars")
@@ -75,15 +75,12 @@ class TestCreateSecretTfvarsFile:
 
 
 class TestSetupEnv:
-
     def test_setupenv_sets_correct_api_key_when_input_as_arg(self):
-        test_api_key = 'test'
-        test_queue_name = 'test_queue_name'
         with SetupEnv(api_key=test_api_key, queue_name=test_queue_name):
             assert os.getenv('api_key') == 'test'
 
     def test_setupenv_sets_correct_queue_name_when_input_as_arg(self):
-        test_api_key = 'test'
-        test_queue_name = 'test_queue_name'
         with SetupEnv(api_key=test_api_key, queue_name=test_queue_name):
             assert os.getenv('queue_name') == 'test_queue_name.fifo'
+
+    # def test_setupenv_deletes_envs_after_they_have_been_setup_correctly(self):
