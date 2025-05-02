e8de694adc2a620caa4c7eea46ec0ed6425cd937
marhide <louis.smith@hyperoceanic.com>
2025-05-02 02:18:42 +0100

    changed sqs queue retention time to 3 days in terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,7 +2,7 @@ resource "aws_sqs_queue" "terraform_queue" {
   name                      = var.queue_name
   delay_seconds             = 1
   max_message_size          = 16384
-  message_retention_seconds = 86400
+  message_retention_seconds = 259200
   receive_wait_time_seconds = 1
   fifo_queue                  = true
   content_based_deduplication = true
